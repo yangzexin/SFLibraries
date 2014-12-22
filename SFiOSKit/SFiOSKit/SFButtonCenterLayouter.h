22dafe0fdd5ec1f95dba1df83d73f05ea74b0f91
yangzexin-imac <yangzexin2011@gmail.com>
2014-12-22 16:28:22 +0800

    add buttoncenterlaupouter

---
+++ SFiOSKit/SFiOSKit/SFButtonCenterLayouter.h
@@ -0,0 +1,17 @@
+//
+//  SFButtonCenterLayouter.h
+//  SFiOSKit
+//
+//  Created by yangzexin on 12/22/14.
+//  Copyright (c) 2014 yangzexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SFIBCompatibleView.h"
+
+@interface SFButtonCenterLayouter : SFIBCompatibleView
+
+@property (nonatomic, assign) CGFloat spacing;
+
+@end
---
+++ SFiOSKit/SFiOSKit/SFButtonCenterLayouter.m
@@ -0,0 +1,27 @@
+//
+//  SFButtonCenterLayouter.m
+//  SFiOSKit
+//
+//  Created by yangzexin on 12/22/14.
+//  Copyright (c) 2014 yangzexin. All rights reserved.
+//
+
+#import "SFButtonCenterLayouter.h"
+
+@implementation SFButtonCenterLayouter
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    if ([[self subviews] count] != 0) {
+        UIButton *button = [[self subviews] lastObject];
+        if ([button isKindOfClass:[UIButton class]]) {
+            CGSize imageSize = [button imageForState:UIControlStateNormal].size;
+            CGSize titleSize = [[button currentTitle] sf_sizeWithFont:button.titleLabel.font];
+            button.imageEdgeInsets = UIEdgeInsetsMake(-(titleSize.height + self.spacing), 0.0, 0.0, -titleSize.width);
+            button.titleEdgeInsets = UIEdgeInsetsMake(0.0, -imageSize.width, -(imageSize.height + self.spacing), 0.0);
+        }
+    }
+}
+
+@end
