d3585cc04aa18f0c1ed9f4c524a00769f9500ac0
yangzexin-imac <yangzexin2011@gmail.com>
2015-05-25 13:47:47 +0800

    autoposition for fieldgroupmanager

--- SFiOSKit/SFiOSKit/SFFieldGroupManager.h
+++ SFiOSKit/SFiOSKit/SFFieldGroupManager.h
@@ -8,16 +8,52 @@
 
 #import <Foundation/Foundation.h>
 
+@interface UITextField (GapBetweenKeyboard)
+
+- (UITextField *)sf_setGapBetweenKeyboard:(CGFloat)gap;
+- (CGFloat)sf_gapBetweenKeyboard;
+
+@end
+
+@interface UITextView (GapBetweenKeyboard)
+
+- (UITextView *)sf_setGapBetweenKeyboard:(CGFloat)gap;
+- (CGFloat)sf_gapBetweenKeyboard;
+
+@end
+
 @interface SFFieldGroupManager : NSObject
 
-@property (nonatomic, readonly) NSArray *fields;
+/**
+ default is YES
+ */
 @property (nonatomic, assign) BOOL setReturnKeyAutomatically;
+
+/**
+ default is YES
+ */
+@property (nonatomic, assign) BOOL setPositionAutomatically;
+
+/**
+ default is NO
+ */
+@property (nonatomic, assign) BOOL alwaysAutoSetPosition;
+
 @property (nonatomic, assign) id<UITextFieldDelegate> textFieldDelegate;
 
 @property (nonatomic, assign) UIReturnKeyType doneReturnKeyType;
 
 @property (nonatomic, copy) void(^whenFieldBecameFirstResponder)(id field);
 @property (nonatomic, copy) void(^whenReturnDone)();
+
+@property (nonatomic, readonly) NSArray *fields;
+
++ (instancetype)manager;
+
+/**
+ The y position of rootScrollView must be 0.0f
+ */
++ (instancetype)managerForRootScrollView:(UIScrollView *)rootScrollView;
 
 - (void)resignFirstResponder;
 - (void)becomeFirstResponder;
--- SFiOSKit/SFiOSKit/SFFieldGroupManager.m
+++ SFiOSKit/SFiOSKit/SFFieldGroupManager.m
@@ -7,17 +7,79 @@
 //
 
 #import "SFFieldGroupManager.h"
+
 #import "SFWaiting.h"
+#import "SFMarkWaiting.h"
 #import "UIView+SFAddition.h"
+#import "NSObject+SFRuntime.h"
+#import "NSObject+SFObjectAssociation.h"
+
+@implementation UITextField (GapBetweenKeyboard)
+
+- (UITextField *)sf_setGapBetweenKeyboard:(CGFloat)gap
+{
+    [self sf_setAssociatedObject:@(gap) key:@"_gapBetweenKeyboard"];
+    
+    return self;
+}
+
+- (CGFloat)sf_gapBetweenKeyboard
+{
+    NSNumber *gap = [self sf_associatedObjectWithKey:@"_gapBetweenKeyboard"];
+    
+    return gap == nil ? .0f : gap.floatValue;
+}
+
+@end
+
+@implementation UITextView (GapBetweenKeyboard)
+
+- (UITextView *)sf_setGapBetweenKeyboard:(CGFloat)gap
+{
+    [self sf_setAssociatedObject:@(gap) key:@"_gapBetweenKeyboard"];
+    
+    return self;
+}
+
+- (CGFloat)sf_gapBetweenKeyboard
+{
+    NSNumber *gap = [self sf_associatedObjectWithKey:@"_gapBetweenKeyboard"];
+    
+    return gap == nil ? .0f : gap.floatValue;
+}
+
+@end
 
 @interface SFFieldGroupManager () <UITextFieldDelegate>
+
+@property (nonatomic, weak) UIScrollView *rootScrollView;
 
 @property (nonatomic, strong) NSMutableArray *addedFields;
 @property (nonatomic, assign) BOOL animating;
 @property (nonatomic, strong) SFWaiting *waitingForNotAnimating;
+@property (nonatomic, strong) SFMarkWaiting *waitingForKeyboardShown;
+
+@property (nonatomic, assign) CGFloat topSuperViewLastPositionY;
+@property (nonatomic, assign) BOOL topSuperViewPositionReseted;
+
+@property (nonatomic, assign) CGFloat keyboardHeight;
+
 @end
 
 @implementation SFFieldGroupManager
+
++ (instancetype)managerForRootScrollView:(UIScrollView *)rootScrollView
+{
+    SFFieldGroupManager *manager = [SFFieldGroupManager new];
+    manager.rootScrollView = rootScrollView;
+    
+    return manager;
+}
+
++ (instancetype)manager
+{
+    return [self managerForRootScrollView:nil];
+}
 
 - (id)init
 {
@@ -27,12 +89,39 @@
     
     self.doneReturnKeyType = UIReturnKeyDone;
     self.setReturnKeyAutomatically = YES;
+    self.setPositionAutomatically = YES;
 
     __weak typeof(self) weakSelf = self;
     self.waitingForNotAnimating = [SFWaiting waitWithCondition:^BOOL{
         __strong typeof(weakSelf) self = weakSelf;
         return !self.animating;
     }];
+    
+    self.waitingForKeyboardShown = [SFMarkWaiting markWaiting];
+    
+    [self sf_depositNotificationObserver:[[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillShowNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+        __strong typeof(weakSelf) self = weakSelf;
+        [self.waitingForKeyboardShown markAsFinish];
+        self.keyboardHeight = [[[note userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+        
+        id field = nil;
+        if ([self _isFirstResponderWithOutField:&field]) {
+            [self fieldWillBeginEditing:field];
+        }
+    }]];
+    
+    [self sf_depositNotificationObserver:[[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillHideNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+        __strong typeof(weakSelf) self = weakSelf;
+        [self.waitingForKeyboardShown resetMark];
+    }]];
+    
+    [self sf_depositNotificationObserver:[[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardDidHideNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+        __strong typeof(weakSelf) self = weakSelf;
+        id field = nil;
+        if ([self _isFirstResponderWithOutField:&field]) {
+            [self _restoreSuperViewPositionWithField:field];
+        }
+    }]];
     
     return self;
 }
@@ -84,33 +173,114 @@
 
 - (BOOL)isFirstResponder
 {
+    return [self _isFirstResponderWithOutField:nil];
+}
+
+- (BOOL)_isFirstResponderWithOutField:(id *)outField
+{
     BOOL isFirstResponder = NO;
     for (UITextField *field in self.fields) {
         if ([field isFirstResponder]) {
             isFirstResponder = YES;
+            if (outField != nil) {
+                *outField = field;
+            }
+            
             break;
         }
     }
+    
     return isFirstResponder;
 }
 
 - (void)fieldWillBeginEditing:(id)field
 {
-    self.animating = YES;
-    [UIView animateWithDuration:.25f animations:^{
-        if (self.whenFieldBecameFirstResponder) {
-            self.whenFieldBecameFirstResponder(field);
+    if (self.whenFieldBecameFirstResponder) {
+        self.whenFieldBecameFirstResponder(field);
+    }
+    if (self.setReturnKeyAutomatically && [field isKindOfClass:[UITextField class]]) {
+        NSUInteger index = [self.fields indexOfObject:field];
+        if (index != NSNotFound) {
+            UITextField *textField = field;
+            textField.returnKeyType = index == self.fields.count - 1 ? self.doneReturnKeyType : UIReturnKeyNext;
         }
-        if (self.setReturnKeyAutomatically && [field isKindOfClass:[UITextField class]]) {
-            NSUInteger index = [self.fields indexOfObject:field];
-            if (index != NSNotFound) {
-                UITextField *textField = field;
-                textField.returnKeyType = index == self.fields.count - 1 ? self.doneReturnKeyType : UIReturnKeyNext;
+    }
+    
+    __weak typeof(self) weakSelf = self;
+    [self.waitingForKeyboardShown wait:^{
+        __strong typeof(weakSelf) self = weakSelf;
+        if (self.setPositionAutomatically) {
+            CGFloat minimalGapHeight = [self _minimalGapHeightForField:field];
+            CGRect fieldFrame = [field frame];
+            UIView *tmpView = [field superview];
+            
+            if (self.rootScrollView == nil) {
+                UIView *topSuperView = [[field sf_viewController] view];
+                CGFloat absoluteY = fieldFrame.origin.y;
+                while (tmpView != topSuperView) {
+                    absoluteY += tmpView.frame.origin.y;
+                    tmpView = tmpView.superview;
+                }
+                
+                CGFloat gap = topSuperView.frame.size.height - absoluteY - self.keyboardHeight - fieldFrame.size.height;
+                
+                if (gap < minimalGapHeight || self.alwaysAutoSetPosition) {
+                    self.topSuperViewPositionReseted = YES;
+                    self.animating = YES;
+                    [UIView animateWithDuration:.25f animations:^{
+                        CGRect tmpFrame = topSuperView.frame;
+                        tmpFrame.origin.y = self.topSuperViewLastPositionY - (minimalGapHeight - gap);
+                        topSuperView.frame = tmpFrame;
+                    } completion:^(BOOL finished) {
+                        self.animating = NO;
+                    }];
+                }
+            } else {
+                CGFloat relativeY = fieldFrame.origin.y;
+                while (tmpView != self.rootScrollView) {
+                    relativeY += tmpView.frame.origin.y;
+                    tmpView = tmpView.superview;
+                }
+                
+                CGFloat gap = self.rootScrollView.frame.size.height - (relativeY - self.rootScrollView.contentOffset.y) - self.keyboardHeight - fieldFrame.size.height;
+                
+                if (gap < minimalGapHeight || self.alwaysAutoSetPosition) {
+                    self.topSuperViewPositionReseted = YES;
+                    self.animating = YES;
+                    [UIView animateWithDuration:.25f animations:^{
+                        CGPoint contentOffset = self.rootScrollView.contentOffset;
+                        contentOffset.y = self.rootScrollView.contentOffset.y + (minimalGapHeight - gap);
+                        self.rootScrollView.contentOffset = contentOffset;
+                    } completion:^(BOOL finished) {
+                        self.animating = NO;
+                    }];
+                }
             }
         }
-    } completion:^(BOOL finished) {
-        self.animating = NO;
     }];
+}
+
+- (CGFloat)_minimalGapHeightForField:(id)field
+{
+    return [field sf_gapBetweenKeyboard];
+}
+
+- (void)_restoreSuperViewPositionWithField:(id)field
+{
+    if (self.setPositionAutomatically && self.topSuperViewPositionReseted) {
+        UIView *topSuperView = [[field sf_viewController] view];
+        [UIView animateWithDuration:.25f animations:^{
+            if (!self.rootScrollView) {
+                CGRect tmpFrame = topSuperView.frame;
+                tmpFrame.origin.y = self.topSuperViewLastPositionY;
+                topSuperView.frame = tmpFrame;
+            } else {
+                CGPoint contentOffset = self.rootScrollView.contentOffset;
+                contentOffset.y = self.topSuperViewLastPositionY;
+                self.rootScrollView.contentOffset = contentOffset;
+            }
+        }];
+    }
 }
 
 - (void)fieldDidEndEditing:(id)field
@@ -119,11 +289,11 @@
     [self.waitingForNotAnimating wait:^{
         __strong typeof(weakSelf) self = weakSelf;
         if (![self isFirstResponder]) {
-            [UIView animateWithDuration:0.25f animations:^{
-                if (self.whenFieldBecameFirstResponder) {
-                    self.whenFieldBecameFirstResponder(nil);
-                }
-            }];
+            if (self.whenFieldBecameFirstResponder) {
+                self.whenFieldBecameFirstResponder(nil);
+            }
+            
+            [self _restoreSuperViewPositionWithField:field];
         }
     } uniqueIdentifier:@"QuickNext"];
 }
@@ -191,6 +361,15 @@
     BOOL shouldBegin = YES;
     if ([_textFieldDelegate respondsToSelector:@selector(textFieldShouldBeginEditing:)]) {
         shouldBegin = [_textFieldDelegate textFieldShouldBeginEditing:textField];
+    }
+    
+    if (self.setPositionAutomatically && ![self isFirstResponder]) {
+        if (self.rootScrollView == nil) {
+            UIView *topSuperView = [[textField sf_viewController] view];
+            self.topSuperViewLastPositionY = topSuperView.frame.origin.y;
+        } else {
+            self.topSuperViewLastPositionY = self.rootScrollView.contentOffset.y;
+        }
     }
     
     return shouldBegin;
